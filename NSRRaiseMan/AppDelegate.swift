ce94d08b09b6d5935953eb15b47c63cd0886e5db
Nasir <momin96@gmail.com>
2018-03-27 13:58:00 +0530

    Initial Commit

---
+++ NSRRaiseMan/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  NSRRaiseMan
+//
+//  Created by Nasir Ahmed Momin on 27/03/18.
+//  Copyright © 2018 Nasir Ahmed Momin. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
